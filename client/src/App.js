bec95c5a0de4ab781b339e5bafaa45dd590e60c0
Dubey <abhidubey@teksystems.com>
2023-04-06 15:02:02 +0530

    client dockerize

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,25 @@
-import "./App.css";
+import axios from "axios";
+import React from "react";
 
-function App() {
+const baseURL = process.env.REACT_APP_METRICS_URL;
+
+export default function App() {
+  const [metrics, setMetrics] = React.useState(null);
+
+  React.useEffect(() => {
+    axios.get(`${baseURL}/metrics`).then((response) => {
+      setMetrics(response.data);
+    });
+  }, []);
+
+  if (!metrics) return null;
+
   return (
-    <div className="App">
-      <h1>Under Construction</h1>
+    <div>
+      <h1>Performance</h1>
+      <h2>Square : {metrics.square}</h2>
+      <h2>Cube : {metrics.cube}</h2>
+      <h2>Fibonacci : {metrics.fibonnaci}</h2>
     </div>
   );
 }
-
-export default App;
